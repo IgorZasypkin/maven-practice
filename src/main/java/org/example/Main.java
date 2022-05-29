6aae15d10767027b87a08a6bce69340b7dd01b6f
student <student@itpark>
2022-05-29 17:50:45 +0300

    clean MainClass

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,9 +4,5 @@ import com.github.javafaker.Faker;
 
 public class Main {
     public static void main(String[] args) {
-        final Faker faker = new Faker();
-        String anName = faker.animal().name();
-
-        System.out.println(anName);
     }
 }
