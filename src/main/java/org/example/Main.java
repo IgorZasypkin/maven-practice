33a06034eeabd92842a2d29c5b0f7c495c936d73
student <student@itpark>
2022-05-29 17:30:12 +0300

    added Java Faker to MainClass

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,12 @@
 package org.example;
 
+import com.github.javafaker.Faker;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        final Faker faker = new Faker();
+        String anName = faker.animal().name();
 
+        System.out.println(anName);
     }
 }
